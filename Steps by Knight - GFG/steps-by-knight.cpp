23a57a5337b758e7f7609c9eb688100a3e3d9d75
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-28 15:03:24 +0530

    Added solution - LeetHub

--- Steps by Knight - GFG/steps-by-knight.cpp
+++ Steps by Knight - GFG/steps-by-knight.cpp
@@ -21,42 +21,46 @@ class Solution
         return true;
     }
     
-    //Function to find out minimum steps Knight needs to reach target position.
+    // Function to find out minimum steps Knight needs to reach target position.
+    // Just perform BFS on the matrix. When knight reaches x,y that means
+    // it is shortest distance possible from start. So update dist[x][y] and mark visited
 	int minStepToReachTarget(vector<int>&KnightPos,vector<int>&TargetPos,int N)
 	{
 	    int sourceX = KnightPos[0];
 	    int sourceY = KnightPos[1];
 	    int destX = TargetPos[0];
 	    int destY = TargetPos[1];
-	    vector<vector<int>> dist(N+1, vector<int> (N+1,INT_MAX));
+	    
+	    // steps[x][y] gives no of steps to reach [x][y] from source
+	    vector<vector<int>> steps(N+1, vector<int> (N+1,INT_MAX));
 	    vector<vector<bool>> visited(N+1,vector<bool> (N+1,false));
 	    
 	    queue<pair<int,int>> q;
 	    q.push({sourceX,sourceY});
 	    visited[sourceX][sourceY] = true;
-	    dist[sourceX][sourceY] = 0;
+	    steps[sourceX][sourceY] = 0;
 	    
 	    while(!q.empty()){
-	        int xCurr = q.front().first;
-	        int yCurr = q.front().second;
+	        int x = q.front().first;
+	        int y = q.front().second;
 	        q.pop();
 	        for(auto move:moves){
-	            int xPos = xCurr + move.first;
-	            int yPos = yCurr + move.second;
-	            if(isValid(xPos,yPos,N) && !visited[xPos][yPos]){
-	                dist[xPos][yPos] = 1 + dist[xCurr][yCurr];
-	                visited[xPos][yPos] = true;
-	                q.push({xPos,yPos});
+	            int childX = x + move.first;
+	            int childY = y + move.second;
+	            if(isValid(childX,childY,N) && !visited[childX][childY]){
+	                steps[childX][childY] = 1 + steps[x][y];
+	                visited[childX][childY] = true;
+	                q.push({childX,childY});
 	            }
 	        }
-	        if(dist[destX][destY]!=INT_MAX){
+	        if(steps[destX][destY]!=INT_MAX){
 	            break;
 	        }
 	    }
-	    if(dist[destX][destY]==INT_MAX){
+	    if(steps[destX][destY]==INT_MAX){
 	        return -1;
 	    }
-	    return dist[destX][destY];
+	    return steps[destX][destY];
 	}
 };
 
