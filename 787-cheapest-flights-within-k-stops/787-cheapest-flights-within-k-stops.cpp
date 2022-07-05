c0490d97e2c9eeeab1c2406fe171d89c968abd35
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-05 16:22:27 +0530

    Time: 60 ms (41.35%), Space: 16.9 MB (24.51%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -37,7 +37,71 @@
 //     }
 // };
 
-// DIJKSTRA ALGO (HARD)
+// // DIJKSTRA ALGO (HARD)
+// class Solution2 {
+// public:
+    
+//     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+//         // create adjacency list where adj[u] = {v,dist bw u and v}
+//         vector<vector<int>> graph[n];
+//         for(auto it:flights){
+//             int u=it[0], v=it[1], wt=it[2];
+//             graph[u].push_back({v,wt});
+//         }
+//         // dist[] stores minimum distance required to reach node from src.
+//         vector<int> dist(n,INT_MAX);
+//         // stops[] stores minimum no of stops required to reach node from src.
+//         vector<int> stops(n,INT_MAX);
+//         // pq => {dist of node from src, node, no of stops b/w src and node}
+//         priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+//         pq.push({0,src,0});
+        
+//         while(!pq.empty()){
+//             auto it = pq.top();
+//             int currCost = it[0], currNode = it[1], currStops = it[2];
+//             pq.pop();
+            
+//             // update the dist[] with minimum distance to reach the node
+//             // first time visiting a node will be the minimum distance from src to node
+//             if(dist[currNode]==INT_MAX){
+//                 dist[currNode] = currCost;
+//             }
+            
+//             // if destination is reached, no more bfs is required
+//             if(currNode==dst){
+//                 break;
+//             }
+            
+//             // we should also consider paths with lesser stops than current no of stops 
+//             // in case the earlier result runs out of stops before reaching destination
+//             // so consider path with minimum distance and also path with minimum no of stops
+//             if(stops[currNode] < currStops){
+//                 continue;
+//             }
+            
+//             stops[currNode] = currStops;
+            
+//             // since we push currStops+1 in pq while traversing children, currStops+1<=k
+//             if(currStops>k){
+//                 continue;
+//             }
+            
+//             // add child nodes to priority queue
+//             for(auto it:graph[currNode]){
+//                 int child=it[0], wt=it[1];
+//                 int new_cost = currCost + wt;
+//                 pq.push({new_cost,child,currStops+1});
+//             }
+//         }
+        
+//         if(dist[dst]!=INT_MAX){
+//             return dist[dst];
+//         }
+//         return -1;
+//     }
+// };
+
+// MODIFIED DIJKSTRA ALGO (HARD) - MORE READABLE
 class Solution {
 public:
     
@@ -52,45 +116,54 @@ public:
         vector<int> dist(n,INT_MAX);
         // stops[] stores minimum no of stops required to reach node from src.
         vector<int> stops(n,INT_MAX);
+        
+        // no of stops between src and dst = k. so total no of stops from src = k+1.
+        // so priority queue can have values from 0 to k+1.
+        int maxStops = k+1;
+        
         // pq => {dist of node from src, node, no of stops b/w src and node}
         priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+
         pq.push({0,src,0});
+        dist[src] = 0;
+        stops[src] = 0;
         
         while(!pq.empty()){
             auto it = pq.top();
             int currCost = it[0], currNode = it[1], currStops = it[2];
             pq.pop();
-            
-            // update the dist[] with minimum distance to reach the node
-            // first time visiting a node will be the minimum distance from src to node
-            if(dist[currNode]==INT_MAX){
-                dist[currNode] = currCost;
+
+            if(dist[currNode] == INT_MAX){
+                dist[currNode] = currCost;    
             }
             
-            // if destination is reached, no more bfs is required
-            if(currNode==dst){
-                break;
-            }
-            
-            // we should also consider paths with lesser stops than current no of stops 
-            // in case the earlier result runs out of stops before reaching destination
-            // so consider path with minimum distance and also path with minimum no of stops
             if(stops[currNode] < currStops){
                 continue;
             }
+            else {
+                stops[currNode] = currStops;
+            }
             
-            stops[currNode] = currStops;
-            
-            // since we push currStops+1 in pq while traversing children, currStops+1<=k
-            if(currStops>k){
-                continue;
+            if(currNode == dst){
+                break;
             }
             
             // add child nodes to priority queue
             for(auto it:graph[currNode]){
                 int child=it[0], wt=it[1];
-                int new_cost = currCost + wt;
-                pq.push({new_cost,child,currStops+1});
+                int childCost = currCost + wt;
+                int childStops = currStops + 1;
+                // stops in pq can be maximum value of maxStops
+                if(childStops <= maxStops){
+                    if(dist[child] > childCost){
+                        // dist[child] = childCost;
+                        pq.push({childCost, child, childStops});
+                    }
+                    else if(stops[child] > childStops){
+                        // stops[child] = childStops;
+                        pq.push({childCost, child, childStops});
+                    }
+                }
             }
         }
         
