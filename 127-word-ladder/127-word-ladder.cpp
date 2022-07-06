9ed5076bb27d2910afd445fbbb19c6d2b3116c4e
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-06 15:01:46 +0530

    Time: 271 ms (53.17%), Space: 31.7 MB (16.38%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    using psi = pair<string,int>;
+    map<string, vector<string>> adj;
+    
+    void createMapping(int n, vector<string> &wordList){
+        for(auto str:wordList){
+            string temp = str;
+            for(int i=0;i<n;i++){
+                temp[i] = '#';
+                adj[temp].push_back(str);
+                temp[i] = str[i];
+            }
+        }
+    }
+    
+    bool checkEndWord(string &endWord, vector<string> &wordList){
+        for(auto str:wordList){
+            if(str==endWord){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    int ladderLength(string &beginWord, string endWord, vector<string>& wordList) {
+        if(checkEndWord(endWord, wordList) == false){
+            return false;
+        }
+        string curr = beginWord;
+        int n = endWord.length();
+        createMapping(n, wordList);
+        queue <psi> q;
+        q.push({curr,1});
+        
+        unordered_set<string> visited;
+        
+        visited.insert(curr);
+        
+        while(!q.empty()){
+            string currWord = q.front().first;
+            int currCount = q.front().second;
+            q.pop();
+            
+            if(currWord == endWord){
+                return currCount;
+            }
+            
+            string temp = currWord;
+            
+            for(int i=0;i<n;i++){
+                temp[i] = '#';
+                for(auto str:adj[temp]){
+                    if(visited.count(str)==0){
+                        q.push({str,currCount+1});
+                        visited.insert(str);
+                    }
+                }
+                temp[i] = currWord[i];
+            }
+        }
+        return 0;
+    }
+};
