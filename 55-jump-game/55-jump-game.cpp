7dea10d3997d222b70ea0d6c29a7cc37d3e9ae46
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-07 23:22:57 +0530

    Time: 331 ms (20.87%), Space: 49.2 MB (15.68%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int dp[10001];
+    
+    bool solve(vector<int> &nums, int index){
+        if(index>=nums.size()-1){
+            return true;
+        }
+        if(dp[index]!=-1){
+            return dp[index];
+        }
+        for(int i=nums[index];i>=1;i--){
+            int new_index = index + i;
+            if(solve(nums, new_index)==true){
+                return dp[index] = true;
+            }
+        }
+        return dp[index] = false;
+    }
+    
+    bool canJump(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
+        return solve(nums,0);
+    }
+};
