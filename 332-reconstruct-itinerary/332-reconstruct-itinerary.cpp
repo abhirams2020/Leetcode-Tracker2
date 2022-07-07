4cbbc2e76caa8d94c1d32a95baded7822e6a0f71
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-07 20:23:02 +0530

    Time: 137 ms (6.18%), Space: 22.3 MB (7.07%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    // adj[curr station] gives next stations vector
+    // adj[curr station] gives next stations vector in lexicographical order
     unordered_map<string, vector<string>> adj;
     
-    // keep track of no of remaining times we can visited an edge [from,to]
-    map<pair<string,string>, int> visitedEdge;
+    // keep track of no of remaining no of times we can visit an edge [from,to]
+    map<pair<string,string>, int> edgeCount;
     
     vector<string> ans;
     
-    // total no of edges we need to visit
-    int noOfEdges;
-    
     // check if a valid route is found. since we sorted the adjacency list, first result should be final answer
-    bool foundRoute = false;
+    bool foundValidRoute = false;
     
     void createGraph(vector<vector<string>>& tickets) {
         // to get lexicographically smallest result, either sort the adjacency list or sort the tickets pair and add to adj
@@ -22,29 +19,31 @@ public:
         for(auto it:tickets){
             string from = it[0];
             string to = it[1];
-            visitedEdge[{from,to}]++;
+            edgeCount[{from,to}]++; // add edges to edgeCount which counts no of times an edge can be visited
             adj[from].push_back(to);
         }
     }
     
     void dfs(string curr, vector<string>& route){
         // if no more edges left to visit
-        if(visitedEdge.size()==0){
+        if(edgeCount.size()==0){
             ans = route;
-            foundRoute = true;
+            foundValidRoute = true;
             return;
         }
         for(auto child:adj[curr]){
-            if(foundRoute==false && visitedEdge.count({curr,child})){
-                visitedEdge[{curr,child}]--;
-                if(visitedEdge[{curr,child}]==0){
-                    visitedEdge.erase({curr,child});
+            // if valid route has not been found and edge {curr,child} can be visited, add child to route and dfs
+            if(foundValidRoute==false && edgeCount.count({curr,child})){
+                edgeCount[{curr,child}]--;
+                if(edgeCount[{curr,child}]==0){
+                    edgeCount.erase({curr,child});
                 }
                 
                 route.push_back(child);
                 dfs(child,route);
                 
-                visitedEdge[{curr,child}]++;
+                // backtracking
+                edgeCount[{curr,child}]++;
                 route.pop_back();
             }
         }
@@ -53,8 +52,6 @@ public:
     vector<string> findItinerary(vector<vector<string>>& tickets) {
         // create adjacency list and add count of each edge to visitedEdge
         createGraph(tickets);
-        
-        noOfEdges = tickets.size();
         
         vector<string> route;
         
