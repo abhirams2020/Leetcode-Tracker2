2d1266a6c3480698a8d1b77740762e720235a8fa
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-07 23:36:54 +0530

    Time: 120 ms (41.49%), Space: 48.4 MB (77.63%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,27 +1,43 @@
-// USING DYNAMIC PROGRAMMING
+// // USING DYNAMIC PROGRAMMING
+// class Solution {
+// public:
+//     // dp[idx] tells if it is possible to reach end index from idx
+//     int dp[10001];
+    
+//     bool solve(vector<int> &nums, int index){
+//         if(index>=nums.size()-1){
+//             return true;
+//         }
+//         if(dp[index]!=-1){
+//             return dp[index];
+//         }
+//         for(int i=nums[index];i>=1;i--){
+//             int new_index = index + i;
+//             if(solve(nums, new_index)==true){
+//                 return dp[index] = true;
+//             }
+//         }
+//         return dp[index] = false;
+//     }
+    
+//     bool canJump(vector<int>& nums) {
+//         memset(dp,-1,sizeof(dp));
+//         return solve(nums,0);
+//     }
+// };
+
+// USING GREEDY ALGORITHM
 class Solution {
 public:
-    // dp[idx] tells if it is possible to reach end index from idx
-    int dp[10001];
-    
-    bool solve(vector<int> &nums, int index){
-        if(index>=nums.size()-1){
-            return true;
-        }
-        if(dp[index]!=-1){
-            return dp[index];
-        }
-        for(int i=nums[index];i>=1;i--){
-            int new_index = index + i;
-            if(solve(nums, new_index)==true){
-                return dp[index] = true;
+    bool canJump(vector<int>& nums) {
+        // from end check if we are able to reach 1st position
+        int goal = nums.size()-1;
+        for(int i=nums.size()-1;i>=0;i--){
+            if(i+nums[i] >= goal){
+                goal = i;
             }
         }
-        return dp[index] = false;
-    }
-    
-    bool canJump(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        return solve(nums,0);
+        
+        return goal==0;
     }
 };
