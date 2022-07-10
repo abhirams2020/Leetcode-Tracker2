d2ea24e22fea6556eadd6ebdad0038e4b675e137
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-10 17:00:34 +0530

    Time: 626 ms (50.00%), Space: 113.4 MB (50.00%) - LeetHub

--- 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.cpp
+++ 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.cpp
@@ -1,53 +1,53 @@
 class Solution {
 public:
     int latestTimeCatchTheBus(vector<int>& buses, vector<int>& passengers, int capacity) {
-        unordered_set<int> pSet;
+        unordered_set<int> passengerTime;
         vector<vector<int>> ans;
         
         sort(buses.begin(), buses.end());
         sort(passengers.begin(), passengers.end());
         
-        for(auto it:passengers){
-            pSet.insert(it);
+        // add passenger times to a set. answer should not have same time as a passenger
+        for(auto i:passengers){
+            passengerTime.insert(i);
         }
         
-        
-        int ptr=0;
-        
-        for(int k=0;k<buses.size();k++){
+        // for each bus, add passengers in it
+        // ans[bus index] gives the list of passengers in it
+        for(int i=0,k=0;i<buses.size();i++){
             int cap = capacity;
             vector<int> temp;
-            while(ptr<passengers.size() && passengers[ptr]<=buses[k] && cap>0){
-                temp.push_back(passengers[ptr]);
+            while(k<passengers.size() && passengers[k]<=buses[i] && cap>0){
+                temp.push_back(passengers[k]);
                 cap--;
-                ptr++;
+                k++;
             }
             ans.push_back(temp);
         }
         
-        // cout<<"\n";
-        // for(auto v:ans){
-        //     for(auto i:v){
-        //         cout<<i<<" ";
-        //     }
-        //     cout<<"\n";
-        // }
-        // cout<<"\n";
-        
+        // from last bus, check till we find valid time
         for(int i=buses.size()-1;i>=0;i--){
+            // if current bus is empty, return that bus time
             if(ans[i].size()==0){
                 return buses[i];
             }
+            // find a start and end range for the current bus
+            // start time should be time of last person who boarded the previous bus
+            // to find start time, go through passenger array and find the time just less than starting time of current bus
             int start = 0;
             for(int j=0;j<passengers.size() && passengers[j]<ans[i][0];j++){
                 start = passengers[j];
             }
+            // if bus capacity is not reached, end time should be bus time.
+            // if capacity is reached, the last passenger time should be the end time. 
+            // beyond that the new passengers cannot enter the bus.
             int end = buses[i];
             if(ans[i].size()==capacity){
                 end = ans[i][capacity-1];
             }
+            // in end to start+1 range, find last time which is not colliding with another passenger time
             for(int j=end;j>start;j--){
-                if(pSet.count(j)==0){
+                if(passengerTime.count(j)==0){
                     return j;
                 }
             }
