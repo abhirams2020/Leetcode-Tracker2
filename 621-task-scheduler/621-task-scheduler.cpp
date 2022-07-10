fb4e51f6103da43d875f375a61b3cef7ff382f1b
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-10 20:26:42 +0530

    Time: 145 ms (43.55%), Space: 37.9 MB (38.88%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.cpp
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char,int> mp;
+        if(n==0){
+            return tasks.size();
+        }
         
+        unordered_map<char,int> mp;
+        // count occurrence of all tasks
         for(auto i:tasks){
             mp[i]++;
         }
-        
+        // maxh stores the occurrence of each task ready to be done
         priority_queue<int> maxh;
+        // q stores the tasks which is in cooldown
         queue<pair<int,int>> q;
         
+        // first push all tasks count to maxh
         for(auto [k,v]:mp){
             maxh.push(v);
         }
@@ -18,11 +24,13 @@ public:
         
         // exit loop only when both maxh and q is empty
         while(!maxh.empty() || !q.empty()){
+            // from q, push every valid task (t<=currtime) to maxh
             while(!q.empty() && q.front().second<=time){
                 maxh.push(q.front().first);
                 q.pop();
             }
             
+            // from maxh, always pick task with most frequency and add to queue after reducing freq by 1
             if(!maxh.empty()){
                 int count = maxh.top();
                 maxh.pop();
