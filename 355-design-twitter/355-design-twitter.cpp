ea02cf775fd707255610e37c4b9aec1b51296028
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-11 11:27:37 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (49.19%) - LeetHub

---
+++ 355-design-twitter/355-design-twitter.cpp
@@ -0,0 +1,62 @@
+class Twitter {
+public:
+    int time = 0;
+    priority_queue<vector<int>> posts;
+    unordered_map<int, unordered_set<int>> following;
+    
+    Twitter() {
+        
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        posts.push({time, userId, tweetId});
+        time++;
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        vector<int> ans;
+        vector<vector<int>> temp;
+        int count = 0;
+        // for(auto [k,v]:following){
+        //     cout<<k<<" : ";
+        //     for(auto i:v){
+        //         cout<<i<<", ";
+        //     }
+        //     cout<<"\n";
+        // }
+        while(!posts.empty() && count<10){
+            auto it = posts.top();
+            // cout<<it[0]<<" "<<it[1]<<" "<<it[2]<<"\n";
+            
+            if(it[1]==userId || following[userId].count(it[1])){
+                ans.push_back(it[2]);
+                count++;
+            }
+            temp.push_back(posts.top());
+            posts.pop();
+        }
+        for(auto v:temp){
+            posts.push(v);
+        }
+        return ans;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        following[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        if(following[followerId].count(followeeId)){
+            following[followerId].erase(followeeId);
+        }
+    }
+};
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter* obj = new Twitter();
+ * obj->postTweet(userId,tweetId);
+ * vector<int> param_2 = obj->getNewsFeed(userId);
+ * obj->follow(followerId,followeeId);
+ * obj->unfollow(followerId,followeeId);
+ */
