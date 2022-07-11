6b225db9fcbcc962d650ea169d7f2259240c262b
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-12 02:49:59 +0530

    Time: 589 ms (21.77%), Space: 10.3 MB (97.83%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,8 +1,8 @@
-// // Memoization DP by taking curr index and prev index as parameters.
+// // METHOD 1 : Memoization DP by taking curr index and prev index as parameters.
 // class Solution {
 // public:
 //     vector<vector<int>> dp;
-    
+
 //     int solve(vector<int>& nums, int& n, int index, int prevIndex){
 //         if(index==n){
 //             return 0;
@@ -25,7 +25,7 @@
 //     }
 // };
 
-// // Memoization DP by taking only prev index as parameter
+// // METHOD 2 : Memoization DP by taking only prev index as parameter
 // class Solution {
 // public:
 //     // dp[i+1] gives longest increasing subsequence with prevIndex = i
@@ -54,29 +54,54 @@
 //     }
 // };
 
-// Tabulation DP by taking only prev index as parameter
+// // METHOD 3 : Tabulation DP by taking curr index and prev index as parameters.
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n = nums.size();
+//         int dp[n+1][n+1];
+//         memset(dp,-1,sizeof(dp));
+//         // when index==n, result is 0.
+//         for(int prevIndex=-1;prevIndex<=n-1;prevIndex++){
+//             dp[n][prevIndex+1] = 0;
+//         }
+//         for(int index=n-1;index>=0;index--){
+//             for(int prevIndex=index-1;prevIndex>=-1;prevIndex--){
+//                 if(prevIndex==-1 || nums[index]>nums[prevIndex]){
+//                     dp[index][prevIndex+1] = max(1+dp[index+1][index+1], dp[index+1][prevIndex+1]);
+//                 }
+//                 else {
+//                     dp[index][prevIndex+1] = dp[index+1][prevIndex+1];
+//                 }
+//             }
+//         }
+//         // int ans = solve(nums,n,0,-1) = dp[0][-1+1] = dp[0][0]
+//         return dp[0][0];
+//     }
+// };
+
+// METHOD 4 : Tabulation DP by taking only prev index as parameter
 class Solution {
 public:
-    
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        int dp[n+1][n+1];
+        int dp[n+2];
         memset(dp,-1,sizeof(dp));
         // when index==n, result is 0.
-        for(int prevIndex=-1;prevIndex<=n-1;prevIndex++){
-            dp[n][prevIndex+1] = 0;
+        for(int prevIndex=-1;prevIndex+1<n+2;prevIndex++){
+            dp[prevIndex+1] = 0;
         }
         for(int index=n-1;index>=0;index--){
             for(int prevIndex=index-1;prevIndex>=-1;prevIndex--){
                 if(prevIndex==-1 || nums[index]>nums[prevIndex]){
-                    dp[index][prevIndex+1] = max(1+dp[index+1][index+1], dp[index+1][prevIndex+1]);
+                    dp[prevIndex+1] = max(1+dp[index+1], dp[prevIndex+1]);
                 }
                 else {
-                    dp[index][prevIndex+1] = dp[index+1][prevIndex+1];
+                    dp[prevIndex+1] = dp[prevIndex+1];
                 }
             }
         }
-        // int ans = solve(nums,n,0,-1) = dp[0][-1+1] = dp[0][0]
-        return dp[0][0];
+        // int ans = solve(nums,n,0,-1) = dp[-1+1] = dp[0]
+        return dp[0];
     }
 };
