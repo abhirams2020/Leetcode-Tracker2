d7d602f7527a3ca800e238f752b122d8785c7ed5
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-12 01:11:29 +0530

    Time: 707 ms (16.30%), Space: 10.6 MB (24.23%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
+    // dp[i+1] gives longest increasing subsequence with prevIndex = i
+    int dp[2502];
     
     int solve(vector<int>& nums, int& n, int index, int prevIndex){
         if(index==n){
             return 0;
         }
-        if(dp[index][prevIndex+1]!=-1){
-            return dp[index][prevIndex+1];
+        if(dp[prevIndex+1]!=-1){
+            return dp[prevIndex+1];
         }
         int include=0, notInclude=0;
         notInclude = solve(nums,n,index+1,prevIndex);
         if(prevIndex==-1 || nums[index]>nums[prevIndex]){
             include = 1 + solve(nums,n,index+1,index);
         }
-        return dp[index][prevIndex+1] = max(include, notInclude);
+        return dp[prevIndex+1] = max(include, notInclude);
     }
     
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        dp.resize(n+1, vector<int>(n+2,-1));
-        return solve(nums,n,0,-1);
+        memset(dp,-1,sizeof(dp));
+        int ans = solve(nums,n,0,-1);
+        return ans;
     }
 };
