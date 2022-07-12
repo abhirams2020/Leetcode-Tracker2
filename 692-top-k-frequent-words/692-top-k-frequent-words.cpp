f2003ceeca7974053beec45169c54d66fa2f9cf1
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-12 23:30:46 +0530

    Time: 17 ms (67.18%), Space: 12.8 MB (33.61%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -1,24 +1,28 @@
 // USING PRIORITY QUEUE
 class Solution {
 public:
+    // sort in descending order of frequency. for same frequency strings are sorted in ascending order.
+    static bool comp(pair<int,string> &a, pair<int,string> &b){
+        if(a.first==b.first){
+            return a.second < b.second;
+        }
+        return a.first>b.first;
+    }
+    
     vector<string> topKFrequent(vector<string>& words, int k) {
         unordered_map<string, int> mp;
         for(auto s:words){
             mp[s]++;
         }
-        priority_queue<pair<int,string>> pq;
+        vector<pair<int,string>> pq;
         for(auto [key,val]:mp){
-            pq.push({-val,key});
-            if(pq.size()>k){
-                pq.pop();
-            }
+            pq.push_back({val,key});
         }
+        sort(pq.begin(),pq.end(),comp);
         vector<string> ans;
-        for(int i=0;i<k && !pq.empty();i++){
-            ans.push_back(pq.top().second);
-            pq.pop();
+        for(int i=0;i<k;i++){
+            ans.push_back(pq[i].second);
         }
-        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
