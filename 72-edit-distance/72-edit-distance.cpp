ba82e739d6b2db30b32a74ee811a64a3c7c78557
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-12 15:50:44 +0530

    Time: 124 ms (13.75%), Space: 7.6 MB (69.66%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int dp[501][501];
     
+    // solve(s1,s2,i,j) -> min no of operations to make s1 to s2 when s1 has i characters left and s2 has j characters left
+    // each time, we make word1[i-1]==word2[j-1] or remove a character and proceed to next step.
     int solve(string& word1, string& word2, int i, int j){
         // if we reached end of both, it means word1==word2
         if(i==0 && j==0){
@@ -18,15 +20,22 @@ public:
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
+        // if both index characters are equal no operation is done. move both index by 1.
         if(word1[i-1]==word2[j-1]){
             return dp[i][j] = solve(word1,word2,i-1,j-1);
         }
+        // everytime, we find result when the current word1[i] is deleted, replaced or other character is inserted
+        // since we are doing an operation, add 1 to the result.
         
-        int ins = solve(word1,word2,i,j-1);
-        int del = solve(word1,word2,i-1,j);
-        int rep = solve(word1,word2,i-1,j-1);
+        // insert -> word2[j] is inserted to word1. move j but i remains at same character.
+        int ins = 1 + solve(word1,word2,i,j-1);
+        // delete -> word1[i] is deleted. So move i to next index but j remains at same place.
+        int del = 1 + solve(word1,word2,i-1,j);
+        // replace -> word1[i] is replaced with word2[j]. move both i and j to next index.
+        int rep = 1 + solve(word1,word2,i-1,j-1);
         
-        return dp[i][j] = 1 + min({ins,del,rep});
+        // find min of result after all 3 operations and assign to dp[i][j]
+        return dp[i][j] = min({ins,del,rep});
     }
     
     int minDistance(string word1, string word2) {
