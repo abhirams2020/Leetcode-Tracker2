5c3284ffa2b078d7cdd5649e8dc99377c56a491c
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-12 15:31:03 +0530

    Time: 143 ms (11.56%), Space: 7.4 MB (74.58%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -3,14 +3,16 @@ public:
     int dp[501][501];
     
     int solve(string& word1, string& word2, int i, int j, int n1, int n2){
+        // if we reached end of both, it means word1==word2
         if(j>=word2.length() && i>=word1.length()){
             return 0;
         }
+        // if we reached end of word1,  word1[0 to n1-1] == word2[0 to n1-1]. add word2[n1 to n2-1] to word1.
         if(i>=word1.length()){
-            return dp[i][j] = word2.length()-j;
+            return dp[i][j] = (n2-1)-n1+1;
         }
         if(j>=word2.length()){
-            return dp[i][j] = word1.length()-i;
+            return dp[i][j] = (n1-1)-n2+1;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
