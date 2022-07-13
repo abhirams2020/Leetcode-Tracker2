36d05b01e5d68eb1dd1b0b1c7af2e3db2eded991
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-13 13:30:44 +0530

    Time: 3 ms (91.63%), Space: 6.2 MB (93.54%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -7,23 +7,28 @@ public:
         if(i==str.length() && j==target.length()){
             return true;
         }
+        // reached end of str but still target not found
         if(i==str.length()){
             return false;
         }
+        // if target reached end but str is not reached end
         if(j==target.length()){
+            // if there is no * at str[i+1], return false
+            // if str[i+1] is *, means we can skip it and move to i+2.
             if(i+1<str.length() && str[i+1]=='*'){
-                return solve(str,target,i+2,j);
+                return dp[i][j] = solve(str,target,i+2,j);
             }
             return false;
         }
-        // cout<<i<<" "<<j<<" "<<str[i]<<" "<<target[j]<<" "<<prev<<"\n";
+        
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
         
-        // if both pointer at alphabet, check if equal and move both to next
-        // if str pointer is at '*', we can move the j pointer or move i pointer if prev matches str[i]
-        // if str pointer is at '.', move both to next
+        // Case-1 : if str[i] has * next to it, str[i] can be taken 0 times till n times
+        //          in this case, at i, we can skip to i+2 or remain at i and check if str[i] match target[j]
+        // Case-2 : if both pointer at alphabet, check if equal and move both to next
+        // Case-3 : if str pointer is at '.', move both to next
         
         bool ans = false;
         
@@ -44,7 +49,6 @@ public:
     }
     
     bool isMatch(string s, string p) {
-        // cout<<s.length()<<" "<<p.length()<<"\n";
         memset(dp,-1,sizeof(dp));
         return solve(p,s,0,0);    
     }
