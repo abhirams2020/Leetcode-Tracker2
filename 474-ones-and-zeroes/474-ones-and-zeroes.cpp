b87d4d0080aa4879837e61d7dd7ea0064030c5b1
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-13 15:51:36 +0530

    Time: 692 ms (16.54%), Space: 35.9 MB (34.07%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -17,10 +17,10 @@ public:
         return {zero,one};
     }
     
-    int knapsack(vector<string> &strs, int index, int m, int n){
+    int knapsack(vector<pii> &nums, int index, int m, int n){
         // if end of strs is reached, return 0. we need not get exactly m 0s and n 1s
         // objective is to get max size subset of strs with AT MOST m 0s and n 1s.
-        if(index==strs.size()){
+        if((m==0 && n==0) || index==nums.size()){
             return 0;
         }
         if(dp[index][m][n]!=-1){
@@ -28,22 +28,27 @@ public:
         }
         
         // count no of 0 and 1 in curr string
-        auto it = count01(strs[index]);
+        auto it = nums[index];
         int i = it.first, j = it.second;
         
         int include=0, notInclude=0;
         
         // if curr string has less than no of zeros and ones reqd, include it
         if(i<=m && j<=n){
-            include = 1 + knapsack(strs, index+1, m-i, n-j);
+            include = 1 + knapsack(nums, index+1, m-i, n-j);
         }
-        notInclude = knapsack(strs, index+1, m, n);
+        notInclude = knapsack(nums, index+1, m, n);
         
         return dp[index][m][n] = max(include, notInclude);
     }
     
     int findMaxForm(vector<string>& strs, int m, int n) {
         memset(dp,-1,sizeof(dp));
-        return knapsack(strs,0,m,n);
+        vector<pii> nums;
+        for(auto s:strs){
+            auto it = count01(s);
+            nums.push_back(it);
+        }
+        return knapsack(nums,0,m,n);
     }
 };
