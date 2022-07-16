ef89070e9f947ab47aedc08886fc2aa1d7760c2d
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-16 21:06:09 +0530

    Time: 385 ms (84.44%), Space: 8.2 MB (94.51%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -40,60 +40,32 @@ public:
 };
 */
 
-
-// // Tabulation DP start and end index as parameter
-// // Matrix chain multiplication with divide and conquer
-// class Solution {
-// public:
-//     int maxCoins(vector<int>& nums) {
-//         nums.push_back(1);
-//         nums.insert(nums.begin(),1);
-//         int n = nums.size();
-//         int dp[n][n];
-//         memset(dp,0,sizeof(dp));
-
-//         // dont know why i and j taken in this order
-//         for(int i=n-1;i>=0;i--) {
-//             for(int j=0;j<=n-1;j++) {
-//                 if(j-i+1<=2)
-//                     continue;
-                
-//                 int maxVal = 0;
-//                 for(int k=i+1;k<=j-1;k++) {
-//                     int cost = nums[i]*nums[k]*nums[j] + dp[i][k] + dp[k][j];  
-//                     maxVal = max(maxVal,cost);
-//                 }
-//                 dp[i][j] = maxVal;
-//             }
-//         }
-//         // solve(0,n-1) = dp[0][n-1]
-//         return dp[0][n-1];
-//     }
-// };
-
+// Tabulation DP start and end index as parameter
+// Matrix chain multiplication with divide and conquer
 class Solution {
 public:
-    
-    int dp[302][302];
-    
-    int solve(vector<int> &nums, int i, int j){
-        if(j-i+1 <= 2){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int ans = 0;
-        for(int k=i+1;k<=j-1;k++){
-            ans = max(ans, nums[i]*nums[k]*nums[j] + solve(nums,i,k) + solve(nums,k,j));
-        }
-        return dp[i][j] = ans;
-    }
     int maxCoins(vector<int>& nums) {
         nums.push_back(1);
         nums.insert(nums.begin(),1);
         int n = nums.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(nums,0,n-1);
+        int dp[n][n];
+        memset(dp,0,sizeof(dp));
+
+        // dont know why i and j taken in this order
+        for(int i=n-1;i>=0;i--) {
+            for(int j=0;j<=n-1;j++) {
+                if(j-i+1<=2)
+                    continue;
+                
+                int maxVal = 0;
+                for(int k=i+1;k<=j-1;k++) {
+                    int cost = nums[i]*nums[k]*nums[j] + dp[i][k] + dp[k][j];  
+                    maxVal = max(maxVal,cost);
+                }
+                dp[i][j] = maxVal;
+            }
+        }
+        // solve(0,n-1) = dp[0][n-1]
+        return dp[0][n-1];
     }
 };
