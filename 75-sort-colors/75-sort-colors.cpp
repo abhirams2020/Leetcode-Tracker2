6b3e69a600f4f6d3d1b882817d4f325a3ef5cad9
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-18 15:20:02 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (23.39%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        // 0 to l-1 will have all 0s
-        // r+1 to n-1 will have all 2s
-        int n = nums.size();
-        int l=0, r=n-1;
-        int i=0;
-        while(i<=r){
-            if(nums[i]==0){
-                swap(nums[i],nums[l]);
+    void sortColors(vector<int>& arr) {
+        // at every instance after swapping,
+        // 0 to l-1 will have 0
+        // l to i will have 1
+        // r+1 to n-1 will have 2
+        int l=0, r=arr.size()-1;
+        // since we dont move i when arr[i]==2, it is sure that if we are at i, 0 to i-1 will not contain 2.
+        // if arr[i]==0, we swap 0 with 1 at index l. so for arr[i]==0 and arr[i]==1, do i++.
+        for(int i=0; i<=r; ){
+            if(arr[i]==0){
+                swap(arr[i],arr[l]);
                 l++;
                 i++;
             }
-            else if(nums[i]==2){
-                swap(nums[i],nums[r]);
-                r--;
-                // not doing i++ since number which was originally in r could be 0
-                // so check that pos once again for 0 to swap nums[i] and nums[l]
-            }
-            else if(nums[i]==1) {
+            else if(arr[i]==1){
                 i++;
+            }
+            else if(arr[i]==2){
+                swap(arr[i],arr[r]);
+                r--;
             }
         }
     }
