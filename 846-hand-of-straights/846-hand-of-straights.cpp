715ddfb8d4bf75cd5ee63dce742c49fb94d290dc
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-22 18:33:06 +0530

    Time: 137 ms (37.60%), Space: 28.4 MB (35.46%) - LeetHub

--- 846-hand-of-straights/846-hand-of-straights.cpp
+++ 846-hand-of-straights/846-hand-of-straights.cpp
@@ -5,15 +5,21 @@ public:
             return false;
         }
         map<int,int> mp;
+        // every element and its count is added to map
+        // map used since we want to start with smallest number
         for(int i=0;i<hand.size();i++){
             mp[hand[i]]++;
         }
+        // check if all elements can be grouped
         while(!mp.size()==0){
             int start = (*mp.begin()).first;
+            // try to create group starting with smallest in map
             for(int k=0;k<groupSize;k++){
+                // if group cannot be formed, return false
                 if(!mp.count(start+k)){
                     return false;
                 }
+                // if group can be form, reduce 1 from count of mp[start+k]
                 else {
                     mp[start+k]--;
                     if(mp[start+k]==0){
