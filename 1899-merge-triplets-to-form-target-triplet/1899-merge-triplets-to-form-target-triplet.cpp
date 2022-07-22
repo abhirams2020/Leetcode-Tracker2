20078fb5996ffe14beaaf48f98279c0be3c0779d
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-22 19:14:07 +0530

    Time: 942 ms (26.82%), Space: 146.5 MB (36.52%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
@@ -1,5 +1,9 @@
+/*
 class Solution {
 public:
+    // O(m*n) space and O(m*n) time
+    // Find triplets which are less than or equal to target since only they can give target on combining
+    // If triplet value > target values, answer will not be obtained
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
         int maxX=target[0], maxY=target[1], maxZ=target[2];
         
@@ -17,20 +21,45 @@ public:
                 validTriplets.push_back(triplets[i]);
             }
         }
-        
-//         for(auto v:validTriplets){
-//             for(auto i:v){
-//                 cout<<i<<" ";
-//             }
-//             cout<<"\n";
-//         }
-        
         // from valid triplets, find max at each position and assign to maxTriplet
         vector<int> maxTriplet(target.size(), 0);
         
         for(int i=0;i<validTriplets.size();i++){
             for(int j=0;j<validTriplets[0].size();j++){
                 maxTriplet[j] = max(maxTriplet[j], validTriplets[i][j]);
+            }
+        }
+        
+        return maxTriplet==target;
+    }
+};
+*/
+
+class Solution {
+public:
+    // O(1) space and O(m*n) time
+    // Find triplets which are less than or equal to target since only they can give target on combining
+    // If triplet value > target values, answer will not be obtained
+    bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
+        int maxX=target[0], maxY=target[1], maxZ=target[2];
+        
+        vector<vector<int>> validTriplets;
+        
+        // from valid triplets, find max at each position and assign to maxTriplet
+        vector<int> maxTriplet(target.size(), 0);
+        
+        for(int i=0;i<triplets.size();i++){
+            // for each vector check if its values are <= target values
+            int count = 0;
+            for(int j=0; j<triplets[i].size(); j++){
+                if(triplets[i][j]<=target[j]){
+                    count++;
+                }
+            }
+            if(count==target.size()){
+                for(int j=0;j<triplets[i].size(); j++){
+                    maxTriplet[j] = max(maxTriplet[j], triplets[i][j]);
+                }
             }
         }
         
