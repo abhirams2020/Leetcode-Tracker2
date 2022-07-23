dd7003208189e209ff3df8069041a07a56aa5bf0
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-23 15:05:24 +0530

    Time: 20 ms (47.50%), Space: 18.5 MB (36.66%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -3,7 +3,7 @@ public:
     int removeDuplicates(vector<int>& nums) {
         // use 2 pointer. add each unique element at l and move it
         // if duplicate elements are there, move i and keep l at same place
-        // index l indicates that 0 to l has unique elements only.
+        // index l indicates that 0 to l-1 has unique elements only.
         int l=0;
         for(int i=0;i<nums.size();i++){
             if(i==0 || nums[i]!=nums[i-1]){
