2b2b92f6718270eb15fef1bc685d7977cf25a4ef
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-23 13:03:48 +0530

    Time: 24 ms (28.15%), Space: 7.7 MB (39.02%) - LeetHub

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
@@ -8,6 +8,8 @@
  */
 class Solution {
 public:
+    // we cannot delete curr node since we need previous to point to curr address
+    // so copy contents of next to curr, and delete next
     void deleteNode(ListNode* node) {
         ListNode* cur = node->next;
         node->val = node->next->val;
