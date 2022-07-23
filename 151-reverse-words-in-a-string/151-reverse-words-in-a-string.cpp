e2f72ffeadab7b54637be608fa2942a7cd81fad8
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-23 19:05:56 +0530

    Time: 3 ms (94.20%), Space: 8.1 MB (44.63%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,23 +1,60 @@
+/*
 // USING STRINGSTREAM
 class Solution {
 public:
     string reverseWords(string s) {
         stringstream ss;
+        // add words to stringstream
         ss<<s;
         vector<string> v;
         string str;
+        // read each word from stringstream
         while(ss>>str){
             v.push_back(str);
         }
-        
+        // reverse order of words
         reverse(v.begin(),v.end());
-        
+        // add each word to ans with space after it
         string ans;
         for(int i=0;i<v.size();i++){
             ans += v[i] + " ";
         }
+        // remove last space
         ans.pop_back();
         
+        return ans;
+    }
+};
+*/
+
+// FINDING EACH WORD AND ADD TO VECTOR
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<string> words;
+        
+        for(int i=0;i<s.length();){
+            // if space, skip that index
+            if(s[i]==' '){
+                i++;
+                continue;
+            }
+            // if we reach character, add are contiguous chars to a word and add to words[]
+            string word;
+            while(i<s.length() && s[i]!=' '){
+                word.push_back(s[i]);
+                i++;
+            }
+            words.push_back(word);
+        }
+        // reverse order of words
+        reverse(words.begin(), words.end());
+        // add reverse words to ans with space in between
+        string ans = "";
+        for(int i=0;i<words.size();i++){
+            ans += words[i] + " ";
+        }
+        ans.pop_back();
         return ans;
     }
 };
