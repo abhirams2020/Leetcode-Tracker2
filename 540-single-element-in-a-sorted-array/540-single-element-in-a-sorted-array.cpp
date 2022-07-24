b7e33ce84ad531babf135256701f3872ceabc358
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-24 17:44:49 +0530

    Time: 43 ms (46.34%), Space: 22.2 MB (99.75%) - LeetHub

---
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        // if only 1 single exist and rest are pairs, means array size is odd.
+        // if when we find pair, we can search left and right of pair. choose portion with odd size
+        int n = nums.size();
+        int l=0, r=n-1;
+        int ans = -1;
+        
+        while(l<=r){
+            int m = l + (r-l)/2;
+            // check left for pair
+            if(m>0 && nums[m-1]==nums[m]){
+                // when pair found at (m-1,m) take 3 sections, l to m-2, m-1 to m, m+1 to r
+                int leftSize = (m-2)-l+1;
+                int rightSize = r-(m+1)+1;
+                // if left of pair is odd, choose that
+                if(leftSize%2!=0){
+                    r = m-2;
+                }
+                else {
+                    l = m+1;
+                }
+            }
+            // check right for pair
+            else if(m<n-1 && nums[m]==nums[m+1]){
+                // when pair found at (m-1,m) take 3 sections, l to m-1, m to m+1, m+2 to r
+                int leftSize = (m-1)-l+1;
+                int rightSize = r-(m+2)+1;
+                // if left of pair is odd, choose that
+                if(leftSize%2!=0){
+                    r = m-1;
+                }
+                else {
+                    l = m+2;
+                }                
+            }
+            else {
+                ans = nums[m];
+                break;
+            }
+        }
+        
+        return ans;
+    }
+};
