a761bfee11995b7e14e98108d83c1a697b611739
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-24 23:54:01 +0530

    Time: 13 ms (48.91%), Space: 13.7 MB (63.15%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int upperBound (vector<int>& nums, int target){
+    int lastPos (vector<int>& nums, int target){
         if(nums.size()==0){
             return -1;
         }        
@@ -11,14 +11,14 @@ public:
             int mid = lo + (hi - lo) / 2;
             // split search space into l to mid and mid+1 to hi
             // if nums[mid]==target, we have to include in search space and go right
-            if(nums[mid] <= target){
-                lo = mid;
-            }
-            else {
+            if(nums[mid] > target){
                 hi = mid-1;
             }
-        }
-        // cout<<nums[lo]<<" "<<nums[hi]<<"\n";
+            else {
+                lo = mid;
+            }
+        }        
+        // since we want last occurance, check from right to left
         if(nums[hi]==target){
             return hi;
         }
@@ -28,25 +28,25 @@ public:
         return -1;
     }
     
-    int lowerBound (vector<int>& nums, int target){
+    int firstPos (vector<int>& nums, int target){
         if(nums.size()==0){
             return -1;
         }
         int n = nums.size();
         int lo=0, hi=n-1;
-        // binary search and reduce search space to 2 (lo,hi)
+        // binary search and reduce search space to 2 (lo,hi) [from luv video]
         while(hi-lo>1){
             int mid = lo + (hi - lo) / 2;
             // split search space into l to mid and mid+1 to hi
             // if nums[mid]==target, we have to include in search space and go left
-            if(nums[mid] >= target){
-                hi = mid;
-            }
-            else {
+            if(nums[mid] < target){
                 lo = mid + 1;
             }
+            else {
+                hi = mid;
+            }
         }
-        // cout<<nums[lo]<<" "<<nums[hi]<<"\n";
+        // since we want first occurance, check from left to right
         if(nums[lo]==target){
             return lo;
         }
@@ -57,8 +57,8 @@ public:
     }
     
     vector<int> searchRange(vector<int>& nums, int target) {
-        int l = lowerBound(nums,target);
-        int u = upperBound(nums,target);
-        return {l,u};
+        int first = firstPos(nums,target);
+        int last = lastPos(nums,target);
+        return {first,last};
     }
 };
