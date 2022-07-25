5ad519821ebbafb2d08d6751fc9d9adf6165bce8
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-26 00:04:16 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (10.10%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
-    bool isValid(string s) {
+    bool isValid(string str) {
+        // if string length odd, cannot pair all
+        if(str.length()%2){
+            return false;
+        }
         stack<char> st;
-        unordered_set<char> open = {'{', '[', '('};
-        unordered_set<char> close = {'}', ']', ')'};
-        unordered_map<char,char> bracketPair = {{'}', '{'}, {']', '['}, {')', '('}};
-        for(auto ch:s){
-            if(open.count(ch)){
+        unordered_set<char> open = {'(', '[', '{'};
+        unordered_set<char> close = {')', ']', '}'};
+        // mapping of opening to corresponding closing bracket
+        unordered_map<char,char> mp = {{')','('}, {']','[' }, {'}','{'}};
+        
+        for(char ch:str) {
+            if(open.count(ch)) {
                 st.push(ch);
             }
             else if(close.count(ch)){
-                if(!st.empty() && st.top() == bracketPair[ch]){
-                    st.pop();
-                }
-                else {
+                // when ch is closing, we need stack.top(). if empty or not matching, return null
+                if(st.empty() || st.top()!=mp[ch]){
                     return false;
                 }
+                st.pop();
             }
         }
-        return st.empty();
+        // if any unpaired opening bracket left, not valid
+        if(!st.empty()){
+            return false;
+        }
+        return true;
     }
 };
