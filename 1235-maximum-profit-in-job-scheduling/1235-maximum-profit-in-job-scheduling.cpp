cf88742e18b67e1d19bc8718f6ecb811b158951d
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-27 11:07:46 +0530

    Time: 304 ms (36.84%), Space: 59.9 MB (45.37%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    vector<int>dp;
-    int find(vector<vector<int>>& events,int idx,int end){
-        // We reach final index, return 0
-        if(idx==events.size())
+    vector<int> dp;
+    
+    int solve(vector<vector<int>>& jobs, int i, int n, int prevEnd){
+        if(i==n){
             return 0;
-        // If the current start time is less than previous end time then we can't attend this event, we move forward.
-        if( events[idx][0] < end )
-            return find( events, idx + 1 , end );
-        if( dp[idx] != -1)
-            return dp[idx];
-        // Return pre stored value if we have 
-        int res= max ( events[idx][2] + find(events, idx + 1 , events[idx][1] ),  find(events , idx + 1 , end ) );
-        return dp[idx] = res;
-    }
-    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit)
-    {
-        //putting startTime,endTimeand profit in an array so that we can sort it
-        vector<vector<int>>events(startTime.size(),vector<int>(3,0));
-        for(int i=0;i<startTime.size();i++)
-        {
-            events[i][0]=startTime[i];
-            events[i][1]=endTime[i];
-            events[i][2]=profit[i];
         }
-        sort(events.begin() , events.end());
-        int n = events.size();
-        //Intialising DP and assigning value -1
-        dp.resize( n,-1);
-        int res = find(events , 0 , 0);
-        // for(auto i:dp){
-        //     cout<<i<<" ";
-        // }
-        // cout<<"\n";
-        return res;
+        if(jobs[i][0]<prevEnd){
+            return solve(jobs,i+1,n,prevEnd);
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        return dp[i] = max(jobs[i][2] + solve(jobs,i+1,n,jobs[i][1]), solve(jobs,i+1,n,prevEnd));
+    }
+    
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int n = startTime.size();
+        dp.resize(n+1,-1);
+        vector<vector<int>> jobs;
+        for(int i=0;i<n;i++){
+            jobs.push_back({startTime[i], endTime[i], profit[i]});
+        }
+        sort(jobs.begin(), jobs.end());
+        int maxProfit = solve(jobs,0,n,0);
+        for(auto i:dp){
+            cout<<i<<"\t";
+        }
+        cout<<"\n";
+        return maxProfit;
     }
 };
