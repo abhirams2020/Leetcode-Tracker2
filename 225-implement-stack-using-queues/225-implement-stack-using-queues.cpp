3222f31b03531cd5bf97e62f5c6519937c1c5a6a
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-27 14:15:16 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (87.01%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -1,55 +1,35 @@
 class MyStack {
 public:
-    queue<int> q1;
-    queue<int> q2;
+    queue<int> q;
     
     MyStack() {
         
     }
     
     void push(int x) {
-        q1.push(x);
+        // we need to add x to front of queue
+        // so add x to back and move the front elements to back of queue
+        int n = q.size();
+        q.push(x);
+        while(n--){
+            int curr = q.front();
+            q.push(curr);
+            q.pop();
+        }
     }
     
     int pop() {
-        // pop last added in stack. ie last in queue
-        // so remove all elements of q till we have only 1 element remaining
-        while(q1.size()>1){
-            int curr = q1.front();
-            q2.push(curr);
-            q1.pop();
-        }
-        int ans = q1.front();
-        q1.pop();
-        while(!q2.empty()){
-            int curr = q2.front();
-            q1.push(curr);
-            q2.pop();
-        }
+        int ans = q.front();
+        q.pop();
         return ans;
     }
     
     int top() {
-         // top is last added in stack. ie last in queue
-        // so remove all elements of q till we have only 1 element remaining
-        while(q1.size()>1){
-            int curr = q1.front();
-            q2.push(curr);
-            q1.pop();
-        }
-        int ans = q1.front();
-        q2.push(ans);
-        q1.pop();
-        while(!q2.empty()){
-            int curr = q2.front();
-            q1.push(curr);
-            q2.pop();
-        }
-        return ans;       
+        return q.front();       
     }
     
     bool empty() {
-        return q1.empty();
+        return q.empty();
     }
 };
 
