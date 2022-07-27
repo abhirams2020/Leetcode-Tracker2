ea7bb8cc795b109257f450bf1ba6751a6d61f8ed
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-27 14:16:36 +0530

    Time: 3 ms (42.42%), Space: 6.7 MB (87.01%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -1,3 +1,62 @@
+/*
+// USING 2 QUEUES
+class MyStack {
+public:
+    queue<int> q1;
+    queue<int> q2;
+    
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q1.push(x);
+    }
+    
+    int pop() {
+        // pop last added in stack. ie last in queue
+        // so remove all elements of q till we have only 1 element remaining
+        while(q1.size()>1){
+            int curr = q1.front();
+            q2.push(curr);
+            q1.pop();
+        }
+        int ans = q1.front();
+        q1.pop();
+        while(!q2.empty()){
+            int curr = q2.front();
+            q1.push(curr);
+            q2.pop();
+        }
+        return ans;
+    }
+    
+    int top() {
+        // top is last added in stack. ie last in queue
+        // so remove all elements of q till we have only 1 element remaining
+        while(q1.size()>1){
+            int curr = q1.front();
+            q2.push(curr);
+            q1.pop();
+        }
+        int ans = q1.front();
+        q2.push(ans);
+        q1.pop();
+        while(!q2.empty()){
+            int curr = q2.front();
+            q1.push(curr);
+            q2.pop();
+        }
+        return ans;       
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
+*/
+
+// USING 1 QUEUE AND MAKING PUSH O(N)
 class MyStack {
 public:
     queue<int> q;
