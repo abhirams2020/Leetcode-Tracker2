d66593dd8961b0d122c01d510a89a5d2be2e5256
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-29 01:26:29 +0530

    Time: 90 ms (17.66%), Space: 121.2 MB (16.54%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -1,3 +1,5 @@
+/*
+// ITERATIVE METHOD
 class Solution {
 public:
     string countAndSay(int n) {
@@ -17,5 +19,30 @@ public:
             s = ans;
         }
         return s;
+    }
+};
+*/
+
+// USING RECURSION FN(N) = COUNT(FN(N-1))
+class Solution {
+public:
+    string solve(string s){
+        string ans;
+        for(int i=0;i<s.length();i++){
+            int count = 1;
+            while(i<s.length()-1 && s[i]==s[i+1]){
+                count++;
+                i++;
+            }
+            ans = ans + to_string(count) + s[i];
+        }
+        return ans;
+    }
+    
+    string countAndSay(int n) {
+        if(n==1){
+            return "1";
+        }
+        return solve(countAndSay(n-1));
     }
 };
