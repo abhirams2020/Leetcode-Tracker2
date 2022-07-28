a6fe6d5758e2cbf0742207c3ac327eda05f944f6
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-29 01:20:15 +0530

    Time: 10 ms (66.20%), Space: 6.7 MB (55.61%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     string countAndSay(int n) {
         string s = "1";
+        // since we already got value for 1, start from 2 till n
         for(int k=2;k<=n;k++){
             string ans = "";
+            // function to count numbers as a sequence
             for(int i=0;i<s.length();i++){
                 int count = 1;
                 while(i<s.length()-1 && s[i]==s[i+1]){
