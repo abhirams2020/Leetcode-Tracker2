113b43dd9edd60edefa7f64594297df22ffb5de7
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-29 02:03:23 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (25.34%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.length()==0){
+            return 0;
+        }
+        if(haystack.length() < needle.length()){
+            return -1;
+        }
+        int n = needle.size();
+        deque<char> needleQ;
+        for(auto c:needle){
+            needleQ.push_back(c);
+        }
+        deque<char> currQ;
+        for(int i=0;i<n;i++){
+            currQ.push_back(haystack[i]);
+        }
+        for(int i=0;i<haystack.size()-n+1;i++){
+            if(currQ == needleQ){
+                return i;
+            }
+            currQ.pop_front();
+            currQ.push_back(haystack[i+n]);
+        }
+        return -1;
+    }
+};
