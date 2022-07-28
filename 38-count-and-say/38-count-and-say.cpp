893c63ad407570993965cd812466f93a92e237df
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-29 01:08:43 +0530

    Time: 103 ms (15.04%), Space: 121.3 MB (16.54%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string solve(string s){
+        string ans;
+        for(int i=0;i<s.length();i++){
+            int count = 1;
+            while(i<s.length()-1 && s[i]==s[i+1]){
+                count++;
+                i++;
+            }
+            ans = ans + to_string(count) + s[i];
+        }
+        return ans;
+    }
+    
+    string countAndSay(int n) {
+        string s = "1";
+        for(int i=2;i<=n;i++){
+            s = solve(s);
+        }
+        return s;
+    }
+};
