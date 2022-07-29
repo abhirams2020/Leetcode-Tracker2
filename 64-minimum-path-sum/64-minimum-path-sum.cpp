637bdf5043a46128d8191e386a3c827df6f4307c
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-29 12:53:02 +0530

    Time: 11 ms (79.53%), Space: 9.8 MB (78.21%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,27 +1,26 @@
-class Solution
-{
-    int rec(int r, int c, int& m, int& n, vector<vector<int>>& grid, vector<vector<int>>& dp)
-    {
-        // Base Cases
-        if(r==m-1 && c==n-1)
-            return grid[r][c];
-        if(r >= m || c >= n)
-            return 1000;  // Random big number
-        if(dp[r][c] != -1)
-            return dp[r][c];
-        
-        int down = grid[r][c] + rec(r+1,c,m,n,grid,dp);
-        int right = grid[r][c] + rec(r,c+1,m,n,grid,dp);
-        
-        
-        return dp[r][c] = min(down,right);
-    }
+class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid)
-    {
+    int dp[201][201];
+    int INF = 1e6;
+    
+    int dfs(vector<vector<int>> &grid, int i, int j, int m, int n){
+        if(i==m-1 && j==n-1) 
+            return grid[i][j];
+        if(i>=m || j>=n){
+            return INF;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int minVal = INF;
+        minVal = min(dfs(grid,i+1,j,m,n), dfs(grid,i,j+1,m,n));
+        return dp[i][j] = grid[i][j] + minVal;
+    }
+    
+    int minPathSum(vector<vector<int>>& grid) {
+        memset(dp,-1,sizeof(dp));
         int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        int ans = rec(0,0,m,n,grid,dp);
-        return ans;
+        int minPath = dfs(grid,0,0,m,n);
+        return minPath;
     }
 };
