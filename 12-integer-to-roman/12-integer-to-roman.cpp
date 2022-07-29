a7bb22822933f1df9ace6db976dfa697a902f1ac
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-29 15:01:22 +0530

    Time: 18 ms (32.19%), Space: 8.9 MB (22.74%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
-    unordered_map<int,string> mp = {{1,"I"}, 
-                                    {4,"IV"},
-                                    {5,"V"}, 
-                                    {9,"IX"},
-                                    {10,"X"}, 
-                                    {40,"XL"}, 
-                                    {50,"L"}, 
-                                    {90,"XC"}, 
-                                    {100,"C"}, 
-                                    {400,"CD"}, 
-                                    {500,"D"}, 
-                                    {900,"CM"},
-                                    {1000,"M"}};
-    
-    string reduce(int &num, int k){
-        string ans;
-        while(num>=k){
-            ans += mp[k];
-            num -= k;
-        }
-        return ans;
-    }
-    
     string intToRoman(int num) {
-        string s = to_string(num);
-        int len = s.length();
-        string ans =    reduce(num,1000) + reduce(num,900) + reduce(num,500) + reduce(num,400) + 
-                        reduce(num,100) + reduce(num,90) + reduce(num,50) + reduce(num,40) + 
-                        reduce(num,10) + reduce(num,9) + reduce(num,5) + reduce(num,4) + reduce(num,1);
+        vector<int> integer = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        vector<string> roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        string ans;
+        for(int i=0;i<integer.size();i++){
+            while(num>=integer[i]){
+                ans += roman[i];
+                num -= integer[i];
+            }
+        }
         return ans;
     }
 };
