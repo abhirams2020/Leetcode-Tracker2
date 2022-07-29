99d654a92d85faec7026bbbe63234cbdca88c5f8
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-07-29 12:48:41 +0530

    Time: 76 ms (5.03%), Space: 14.5 MB (6.02%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -4,16 +4,11 @@ public:
     vector<vector<int>> dir = {{0,1}, {1,0}};
     int INF = 1e6;
     
-    bool isValid(int i, int j, int m, int n){
-        if(i<0 || j<0 || i>=m || j>=n){
-            return false;
-        }
-        return true;
-    }
-    
     int dfs(vector<vector<int>> &grid, int i, int j, int m, int n){
-        if(i==m-1 && j==n-1){
+        if(i==m-1 && j==n-1) 
             return grid[i][j];
+        if(i>=m || j>=n){
+            return INF;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
@@ -21,9 +16,7 @@ public:
         int minVal = INF;
         for(auto it:dir){
             int new_i = i + it[0], new_j = j + it[1];
-            if(isValid(new_i,new_j,m,n)){
-                minVal = min(minVal, grid[i][j] + dfs(grid,new_i,new_j,m,n));
-            }
+            minVal = min(minVal, grid[i][j] + dfs(grid,new_i,new_j,m,n));
         }
         return dp[i][j] = minVal;
     }
