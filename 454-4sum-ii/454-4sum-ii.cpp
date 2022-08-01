b011c109c5b3cf017cdac3b5b520596b3b53f5d9
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-08-01 13:03:16 +0530

    Time: 275 ms (87.28%), Space: 24.4 MB (76.78%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -9,6 +9,8 @@ public:
         
         unordered_map<int,int> mp;
         
+        // for getting n1 + n2 + n3 + n4 = 0, we can take from n1,n2 and from n3,n4 and check
+        // in mp, store the sum possible with n1,n2 and their frequencies
         for(auto i:nums1){
             for(auto j:nums2){
                 mp[i+j]++;
@@ -17,6 +19,7 @@ public:
         
         int count = 0;
         
+        // for n1+n2+n3+n4==0, n3+n4 should give -(n1+n2). so check if -(n3+n4) exist in map.
         for(auto i:nums3){
             for(auto j:nums4){
                 if(mp.count(target-(i+j))){
