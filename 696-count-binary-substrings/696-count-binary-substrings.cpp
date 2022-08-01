4de78f9f55711fbd2c7e422cf195dee5c05767ff
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-08-01 17:42:18 +0530

    Time: 38 ms (33.71%), Space: 10.5 MB (47.31%) - LeetHub

--- 696-count-binary-substrings/696-count-binary-substrings.cpp
+++ 696-count-binary-substrings/696-count-binary-substrings.cpp
@@ -3,6 +3,9 @@ public:
     int countBinarySubstrings(string s) {
         int n = s.length();
         int count = 0;
+        // similar to count consecutive elements
+        // when we find 0,1 consecutive expand to left and right to get max possible substring
+        // for each valid substring increment count
         for(int i=0;i<n-1;i++){
             if(s[i]!=s[i+1]){
                 int left = s[i], right = s[i+1];
