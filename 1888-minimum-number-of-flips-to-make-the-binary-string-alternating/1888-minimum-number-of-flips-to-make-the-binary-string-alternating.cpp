1068e552025cbc791a8bf313ec5ad093135c96b6
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-08-03 01:56:00 +0530

    Time: 77 ms (60.05%), Space: 25.1 MB (33.81%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -8,41 +8,46 @@ public:
         s += s;
         // for each window we compare with the string of same size with alternate numbers
         // alternates can start with 0 or 1. count flips required to convert to each and take min
-        string alt1;
-        string alt2;
-        // create the alternate strings with size = length of new s
-        int currVal = 0;
+        string target1;
+        string target2;
+        // create the target strings with size = length of new s
+        // for target1, at even index add 0, at odd index add 1
         for(int i=0;i<s.length();i++){
-            alt1 += to_string(currVal);
-            alt2 += to_string(1 - currVal);
-            currVal = (currVal+1)%2;
+            if(i%2==0){
+                target1 += '0';
+                target2 += '1';
+            }
+            else {
+                target1 += '1';
+                target2 += '0';
+            }
         }
         
         int left=0, right=0;
-        int count1=0, count2=0, minCount=INT_MAX;
+        int diff1=0, diff2=0, minDiff=INT_MAX;
         while(right<s.length()){
             // add right characters
-            if(s[right]!=alt1[right]){
-                count1++;
+            if(s[right]!=target1[right]){
+                diff1++;
             }
-            if(s[right]!=alt2[right]){
-                count2++;
+            if(s[right]!=target2[right]){
+                diff2++;
             }
             // if l to r window has size == k, update min value and move left ptr by 1
             if(right-left+1 == k){
-                minCount = min({minCount, count1, count2});
-
-                if(s[left]!=alt1[left]){
-                    count1--;
+                minDiff = min({minDiff, diff1, diff2});
+                // move left of window
+                if(s[left]!=target1[left]){
+                    diff1--;
                 }
-                if(s[left]!=alt2[left]){
-                    count2--;
+                if(s[left]!=target2[left]){
+                    diff2--;
                 }
                 left++;
             }
             right++;
         }
         
-        return minCount;
+        return minDiff;
     }
 };
