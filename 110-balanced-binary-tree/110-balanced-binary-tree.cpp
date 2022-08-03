c8585595136d04839659b19588f2161c9b7c2233
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-08-03 12:51:25 +0530

    Time: 23 ms (44.21%), Space: 20.9 MB (81.30%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/*
+// USING SEPERATE FUNCTION FOR FINDING MAX DEPTH OF TREE. FOR EACH WE NEED TO FIND DEPTH. NOT EFFICIENT
 class Solution {
 public:
     int maxTreeDepth(TreeNode* root){
@@ -30,5 +32,34 @@ public:
         }
         // check if left and right subtree is balanced tree
         return isBalanced(root->left) && isBalanced(root->right);
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxTreeDepth(TreeNode* root){
+        if(root==NULL){
+            return 0;
+        }        
+        int leftH = maxTreeDepth(root->left);
+        int rightH = maxTreeDepth(root->right);
+        // if any subtree is not valid, return -1
+        if(leftH==-1 || rightH==-1){
+            return -1;
+        }
+        // if subtrees valid, compare height of both
+        if(abs(leftH-rightH)>1){
+            return -1;
+        }        
+        return 1 + max(leftH, rightH);
+    }
+    
+    bool isBalanced(TreeNode* root) {
+        if(root==NULL){
+            return true;
+        }
+        // -1 means tree is unbalanced. if any of right or left subtree is unbalanced, -1 is returned
+        return maxTreeDepth(root)!=-1;
     }
 };
