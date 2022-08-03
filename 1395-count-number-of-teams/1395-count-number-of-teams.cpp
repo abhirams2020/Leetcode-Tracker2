1e28384ac3d881a6601598049beefd03653f58cf
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-08-03 16:34:31 +0530

    Time: 731 ms (5.01%), Space: 9.2 MB (43.47%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int dp[2][4][1002];
     
+    // similar to knapsack dp problems
     int countTeams(vector<int>& rating, int idx, int prevIdx, int teamSize, bool incr){
         if(teamSize==3){
             return 1;
@@ -15,18 +16,24 @@ public:
         }
         
         int incl = 0, notIncl = 0;
+        // the function should return count of teams when curr is included + count when not included
         if(prevIdx==-1  || (incr==true && rating[prevIdx]<rating[idx]) 
                         || (incr==false && rating[prevIdx]>rating[idx])){
-            incl = countTeams(rating, idx+1, idx, teamSize+1, incr);
+            // when curr is valid and included, prevIdx is idx and teamSize incremented by 1
+            return  dp[incr][teamSize][prevIdx+1] = countTeams(rating, idx+1, idx, teamSize+1, incr) +
+                                                    countTeams(rating, idx+1, prevIdx, teamSize, incr);
         }
-        notIncl = countTeams(rating, idx+1, prevIdx, teamSize, incr);
-        return dp[incr][teamSize][prevIdx+1] = incl + notIncl;
+        else {
+            return dp[incr][teamSize][prevIdx+1] = countTeams(rating, idx+1, prevIdx, teamSize, incr);
+        }
     }
     
     int numTeams(vector<int>& rating) {
         memset(dp,-1,sizeof(dp));
+        
         int incrCount = countTeams(rating,0,-1,0,true);
         int decrCount = countTeams(rating,0,-1,0,false);
-        return incrCount+decrCount;
+        
+        return incrCount + decrCount;
     }
 };
