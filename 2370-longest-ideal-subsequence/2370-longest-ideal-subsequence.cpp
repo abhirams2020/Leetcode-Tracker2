5d17f70e46aebe97d4c9f43d7d94aa5e6a488b21
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-08-07 23:22:06 +0530

    Time: 1497 ms (28.57%), Space: 73.3 MB (85.71%) - LeetHub

---
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int dp[100004][128];
+    
+    int findLongest(string &s, int &k, int i, int prev){
+        if(i==s.length()){
+            return 0;
+        }
+        if(dp[i][prev+1] != -1){
+            return dp[i][prev+1];
+        }
+        int include = 0, notInclude = 0;
+        if(prev==-1 || (abs(s[i]-prev)<=k)){
+            include = 1 + findLongest(s,k,i+1,s[i]);
+            notInclude = findLongest(s,k,i+1,prev);
+        }
+        else {
+            notInclude = findLongest(s,k,i+1,prev);
+        }
+        return dp[i][prev+1] = max(include, notInclude);
+    }
+    
+    int longestIdealString(string &s, int k) {
+        memset(dp,-1,sizeof(dp));
+        return findLongest(s,k,0,-1);
+    }
+};
