51d644bd169d6a488c49f039e4e618b45e9f1c48
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-08-07 22:35:51 +0530

    Time: 376 ms (12.61%), Space: 17.1 MB (51.87%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     long long mod = 1e9+7;
@@ -25,21 +26,51 @@ public:
     }
     
     int countVowelPermutation(int n) {
-        // convert a,e,i,o,u to 1,2,3,4,5
         mp['a'] = {'e'};
         mp['e'] = {'a','i'};
         mp['i'] = {'a','e','o','u'};
         mp['o'] = {'i','u'};
         mp['u'] = {'a'};
-        
-        // for(auto [k,v]:mp){
-        //     cout<<k<<" : ";
-        //     for(auto i:v){
-        //         cout<<i<<" ";
-        //     }
-        //     cout<<"\n";
-        // }
         memset(dp,-1,sizeof(dp));
         return solve(n,'#');
+    }
+};
+*/
+
+class Solution {
+public:
+    long long mod = 1e9+7;
+    
+    unordered_map<char,unordered_set<char>> mp;
+    
+    vector<char> vowels = {'a','e','i','o','u'};
+    
+    int dp[20002][27];
+    
+    int solve(int n, int prev){
+        if(n==0){
+            return 1;
+        }
+        if(dp[n][prev-'a']!=-1){
+            return dp[n][prev-'a'];
+        }
+        int count = 0;
+        for(auto ch:vowels){
+            if(prev-'a'==26 || mp[prev].count(ch)){
+                count = (count + solve(n-1,ch))%mod;
+            }
+        }
+        return dp[n][prev-'a'] = count;
+    }
+    
+    int countVowelPermutation(int n) {
+        mp['a'] = {'e'};
+        mp['e'] = {'a','i'};
+        mp['i'] = {'a','e','o','u'};
+        mp['o'] = {'i','u'};
+        mp['u'] = {'a'};
+        memset(dp,-1,sizeof(dp));
+        char prev = 'a'+26;
+        return solve(n, prev);
     }
 };
