dc547efd494b450ccf3bf07f31f9e67cb319f770
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-08-07 22:52:38 +0530

    Time: 109 ms (41.90%), Space: 15.6 MB (53.27%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,51 +1,11 @@
-/*
+// MEMOIZATION DP. O(5*N) TIME. O(26*N) SPACE.
 class Solution {
 public:
     long long mod = 1e9+7;
     
     unordered_map<char,unordered_set<char>> mp;
     
-    vector<char> vowels = {'a','e','i','o','u'};
-    
-    int dp[20002][128];
-    
-    int solve(int n, int prev){
-        if(n==0){
-            return 1;
-        }
-        if(dp[n][prev]!=-1){
-            return dp[n][prev];
-        }
-        int count = 0;
-        for(auto ch:vowels){
-            if(prev=='#' || mp[prev].count(ch)){
-                count = (count + solve(n-1,ch))%mod;
-            }
-        }
-        return dp[n][prev] = count;
-    }
-    
-    int countVowelPermutation(int n) {
-        mp['a'] = {'e'};
-        mp['e'] = {'a','i'};
-        mp['i'] = {'a','e','o','u'};
-        mp['o'] = {'i','u'};
-        mp['u'] = {'a'};
-        memset(dp,-1,sizeof(dp));
-        return solve(n,'#');
-    }
-};
-*/
-
-class Solution {
-public:
-    long long mod = 1e9+7;
-    
-    unordered_map<char,unordered_set<char>> mp;
-    
-    vector<char> vowels = {'a','e','i','o','u'};
-    
-    int dp[20002][27];
+    int dp[20002][26];
     
     int solve(int n, int prev){
         if(n==0){
@@ -55,22 +15,20 @@ public:
             return dp[n][prev-'a'];
         }
         int count = 0;
-        for(auto ch:vowels){
-            if(prev-'a'==26 || mp[prev].count(ch)){
-                count = (count + solve(n-1,ch))%mod;
-            }
+        for(auto ch:mp[prev]){
+            count = (count + solve(n-1,ch))%mod;
         }
         return dp[n][prev-'a'] = count;
     }
     
     int countVowelPermutation(int n) {
+        mp['n'] = {'a','e','i','o','u'}; // n is for condition when no element included so far
         mp['a'] = {'e'};
         mp['e'] = {'a','i'};
         mp['i'] = {'a','e','o','u'};
         mp['o'] = {'i','u'};
         mp['u'] = {'a'};
         memset(dp,-1,sizeof(dp));
-        char prev = 'a'+26;
-        return solve(n, prev);
+        return solve(n, 'n');
     }
 };
