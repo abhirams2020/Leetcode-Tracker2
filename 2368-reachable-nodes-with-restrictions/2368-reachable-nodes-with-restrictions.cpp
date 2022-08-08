83c99bd3d6e8e2a459c171e7f7639782228e63f1
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-08-08 17:06:42 +0530

    Time: 1252 ms (11.11%), Space: 211.4 MB (11.11%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>> adj;
+    unordered_map<int,vector<int>> adj;
     vector<bool> visited;
+    int count = 0;
     
-    void dfs(int src, unordered_set<int> &restrictedSet){
+    void dfs(int src){
+        // for every valid node that is visited, increment count
+        count++;
         for(auto child:adj[src]){
-            if(visited[child]==false && restrictedSet.count(child)==0){
+            if(visited[child]==false){
                 visited[child] = true;
-                dfs(child, restrictedSet);
+                dfs(child);
             }
         }
     }
     
     int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
-        adj.resize(n);
         visited.resize(n,false);
-        unordered_set<int> restrictedSet;
         for(auto it:edges){
             adj[it[0]].push_back(it[1]);
             adj[it[1]].push_back(it[0]);
         }
+        // mark all restricted nodes as visited to avoid visiting it again during dfs
         for(auto i:restricted){
-            restrictedSet.insert(i);
+            visited[i] = true;
         }
         visited[0] = true;
-        dfs(0,restrictedSet);
-        int count = 0;
-        for(int i=0;i<visited.size();i++){
-            if(visited[i] == true){
-                count++;
-            }
-        }
+        dfs(0);
         return count;
     }
 };
