9204ea6e2bec6da55dfc696dc60216917394cf98
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-08-08 14:10:52 +0530

    Time: 84 ms (54.25%), Space: 39.3 MB (83.73%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
+        
         // we need to find max of val[i]+i + val[j]-j such that i<j
-        // we can store all the val[i] + i before j and find the max out of those using priority queue
-        priority_queue<int> add; // values are val[i] + i
+        // we can store all the val[i] + i before j and find the max out of those
+        // or after visiting each index, store its val[i]+i value if it is greater than previous ones
         
-        int maxVal = INT_MIN;
+        int maxVal = INT_MIN; // to store max value of val[i]+i + val[j]-j
         
+        int prevMaxSum = INT_MIN; // max value of val[i] + i found so far
+        
+        // for every index in arr, check if val[curr] - curr + prevMaxSum greater than maxVal so far
         for(int i=0;i<values.size();i++){
-            if(!add.empty()){
-                maxVal = max(maxVal, add.top() + values[i]-i);
+            if(prevMaxSum != INT_MIN){
+                maxVal = max(maxVal, prevMaxSum + values[i]-i);
             }
-            add.push({values[i] + i});
+            prevMaxSum = max(prevMaxSum, values[i] + i);
         }
         
         return maxVal;
