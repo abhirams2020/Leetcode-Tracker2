bfde9696cfb70807844fc6786519e653873166f3
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-08-08 17:03:45 +0530

    Time: 1106 ms (11.11%), Space: 200.3 MB (11.11%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
@@ -1,75 +1,36 @@
 class Solution {
 public:
-//     unordered_map<int,vector<int>> adj;
-//     vector<bool> visited;
+    vector<vector<int>> adj;
+    vector<bool> visited;
     
-//     void dfs(int src, unordered_set<int> &restrictedSet){
-//         for(auto child:adj[src]){
-//             if(visited[child]==false && restrictedSet.count(child)==0){
-//                 visited[child] = true;
-//                 dfs(child, restrictedSet);
-//             }
-//         }
-//     }
-    
-//     int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
-//         visited.resize(n,false);
-//         unordered_set<int> restrictedSet;
-//         for(auto it:edges){
-//             adj[it[0]].push_back(it[1]);
-//             adj[it[1]].push_back(it[0]);
-//         }
-//         for(auto i:restricted){
-//             restrictedSet.insert(i);
-//         }
-//         visited[0] = true;
-//         dfs(0,restrictedSet);
-//         int count = 0;
-//         for(int i=0;i<visited.size();i++){
-//             if(visited[i] == true){
-//                 count++;
-//             }
-//         }
-//         return count;
-//     }
-    void DFS(int &n, vector<vector<int>> &graph, vector<int> &visited, int sv, int &ans)
-    {
-        visited[sv] = 1;
-        ans++;
-
-        for (int i = 0; i < graph[sv].size(); i++)
-        {
-            int nv = graph[sv][i];
-            if ( visited[nv] == 0 )
-            {
-                DFS(n, graph, visited, nv, ans);
+    void dfs(int src, unordered_set<int> &restrictedSet){
+        for(auto child:adj[src]){
+            if(visited[child]==false && restrictedSet.count(child)==0){
+                visited[child] = true;
+                dfs(child, restrictedSet);
             }
         }
     }
-
-    int reachableNodes(int n, vector<vector<int>> &edges, vector<int> &restricted)
-    {
-        vector<vector<int>> graph(n);
-        vector<int> visited(n, 0);
-
-        for (auto &it : restricted)
-        {
-            visited[it] = 1;
+    
+    int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
+        adj.resize(n);
+        visited.resize(n,false);
+        unordered_set<int> restrictedSet;
+        for(auto it:edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }
-
-        for (int i = 0; i < edges.size(); i++)
-        {
-            int a = edges[i][0];
-            int b = edges[i][1];
-            graph[a].push_back(b);
-            graph[b].push_back(a);
+        for(auto i:restricted){
+            restrictedSet.insert(i);
         }
-
-        // set<int>s(restricted.begin(), restricted.end());
-
-        int ans = 0;
-        DFS(n, graph, visited, 0, ans);
-
-        return ans;
+        visited[0] = true;
+        dfs(0,restrictedSet);
+        int count = 0;
+        for(int i=0;i<visited.size();i++){
+            if(visited[i] == true){
+                count++;
+            }
+        }
+        return count;
     }
 };
