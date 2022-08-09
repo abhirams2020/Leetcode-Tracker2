60a25300dc6deb58268fa9b143f7542efa3228b2
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-08-09 18:40:52 +0530

    Time: 74 ms (38.03%), Space: 38.8 MB (32.56%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -9,30 +9,44 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     unordered_set<int> s;
+//     bool flag = false;
+    
+//     void inorder(TreeNode* root, int target){
+//         if(root==NULL){
+//             return;
+//         }
+//         inorder(root->left,target);
+        
+//         int curr = root->val;
+//         if(s.count(target-curr)){
+//             flag = true;
+//         }
+//         s.insert(curr);
+        
+//         inorder(root->right,target);
+//     }
+    
+//     // add elements to set inorder and check if target-curr exist in set
+//     bool findTarget(TreeNode* root, int k) {
+//         inorder(root, k);
+//         return flag;
+//     }
+// };
 class Solution {
 public:
-    unordered_set<int> s;
-    bool flag = false;
-    
-    void inorder(TreeNode* root, int target){
-        if(root==NULL){
-            return;
-        }
-        inorder(root->left,target);
+    unordered_set<int> mp;
+    bool findTarget(TreeNode* root, int target) {
+        if(root==NULL){return false;}
         
-        int curr = root->val;
-        if(s.count(target-curr)){
-            flag = true;
-            return;
-        }
-        s.insert(curr);
+        if(mp.count(target-(root->val)))
+        {return true;}
         
-        inorder(root->right,target);
-    }
-    
-    // add elements to set inorder and check if target-curr exist in set
-    bool findTarget(TreeNode* root, int k) {
-        inorder(root, k);
-        return flag;
+        mp.insert(root->val);
+        
+        return (findTarget(root->left,target)||findTarget(root->right,target));
+        
     }
 };
