d272e807d854ce8d34a3d3f22ef1f584ac5410a6
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-08-11 05:03:36 +0530

    Time: 154 ms (38.83%), Space: 38 MB (30.86%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.cpp
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -1,45 +1,34 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        if(n==0){
-            return tasks.size();
+        unordered_map<char,int> taskCount;
+        for(auto task:tasks){
+            taskCount[task]++;
         }
-        
-        unordered_map<char,int> mp;
-        // count occurrence of all tasks
-        for(auto i:tasks){
-            mp[i]++;
-        }
-        // maxh stores the occurrence of each task ready to be done
+        // add task counts to a maxheap. any task in maxheap can be executed and add to waiting queue
         priority_queue<int> maxh;
-        // q stores the tasks which is in cooldown
-        queue<pair<int,int>> q;
-        
-        // first push all tasks count to maxh
-        for(auto [k,v]:mp){
-            maxh.push(v);
+        for(auto [task,count]:taskCount){
+            maxh.push(count);
         }
+        // maintain a waiting queue with values {taskcount, next valid time to run task}
+        queue<pair<int,int>> q;
         
         int time = 0;
         
-        // exit loop only when both maxh and q is empty
         while(!maxh.empty() || !q.empty()){
-            // from q, push every valid task (t<=currtime) to maxh
-            while(!q.empty() && q.front().second<=time){
+            // add all valid tasks in queue to maxh
+            while(!q.empty() && q.front().second <= time){
                 maxh.push(q.front().first);
                 q.pop();
             }
-            
-            // from maxh, always pick task with most frequency and add to queue after reducing freq by 1
             if(!maxh.empty()){
-                int count = maxh.top();
+                int currCount = maxh.top();
                 maxh.pop();
-                count--;
-                if(count>0){
-                    q.push({count,time+n+1});
+                // if this is last occurence of current task, no need to add in wait queue
+                if(currCount>1){
+                    q.push({currCount-1, time+n+1});
                 }
             }
-            
             time++;
         }
         
