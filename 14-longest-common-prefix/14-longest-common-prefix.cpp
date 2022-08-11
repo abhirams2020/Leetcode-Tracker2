901a1fcb25b6222ace8dcba4dbcc6336ec9d551d
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-08-11 18:10:14 +0530

    Time: 4 ms (79.32%), Space: 9 MB (94.94%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
+    string longestCommonPrefix(vector<string>& strs) {      
         string ans;
+        bool isMatch=true;
         for(int i=0;i<strs[0].length();i++){
-            char curr = strs[0][i];
-            bool isMatch=true;
+            char curr = strs[0][i]; // take every character from first word and compare 
+            // compare current character with i'th index for words 1 to n-1
             for(int j=1;j<strs.size();j++){
-                if(strs[j][i]!=curr){
+                // if word[j] does not have i'th index or i'th index character different, ismatch = false
+                if(i==strs[j].length() || strs[j][i]!=curr){
                     isMatch = false;
                 }
             }
+            // if ismatch is false, break from the loop
             if(isMatch==true){
                 ans.push_back(curr);
             }
