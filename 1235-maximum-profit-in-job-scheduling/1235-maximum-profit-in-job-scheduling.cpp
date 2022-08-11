56d88f8e7b6876d5bb8a465a6926988a35389829
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-08-11 08:18:19 +0530

    Time: 376 ms (22.99%), Space: 59.9 MB (44.92%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,3 +1,47 @@
+/*
+// USING 2D DP WITH INDEX AND PREV INDEX AS PARAMETERS
+class Solution {
+public:
+    vector<vector<int>> dp;
+    
+    int solve(vector<vector<int>>& jobs, int i, int n, int prevIdx){
+        if(i==n){
+            return 0;
+        }
+        if(dp[i][prevIdx+1]!=-1){
+            return dp[i][prevIdx+1];
+        }
+        int include = 0, notInclude = 0;
+        // start of current job should be more than or equal to end of previous one
+        if(prevIdx==-1 || jobs[i][0] >= jobs[prevIdx][1]){
+            include = jobs[i][2] + solve(jobs,i+1,n,i);
+        }
+        notInclude = solve(jobs,i+1,n,prevIdx);
+        return dp[i][prevIdx+1] = max(include, notInclude);
+    }
+    
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int n = startTime.size();
+        dp.resize(n+1, vector<int> (n+2,-1));
+        vector<vector<int>> jobs;
+        for(int i=0;i<n;i++){
+            jobs.push_back({startTime[i], endTime[i], profit[i]});
+        }
+        sort(jobs.begin(), jobs.end());
+        int maxProfit = solve(jobs,0,n,-1);
+        // by printing dp matrix, we find dp[i][j] only depends on i
+        for(auto v:dp){
+            for(auto i:v){
+                cout<<i<<"\t";
+            }
+            cout<<"\n";
+        }
+        return maxProfit;
+    }
+};
+*/
+
+// USING 1D DP WITH INDEX AS PARAMETER
 class Solution {
 public:
     vector<int> dp;
@@ -24,10 +68,6 @@ public:
         }
         sort(jobs.begin(), jobs.end());
         int maxProfit = solve(jobs,0,n,0);
-        // for(auto i:dp){
-        //     cout<<i<<"\t";
-        // }
-        // cout<<"\n";
         return maxProfit;
     }
 };
