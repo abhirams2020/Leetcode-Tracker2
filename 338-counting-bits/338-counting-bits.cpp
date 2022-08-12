549d085e8cce3d9b59b17e1a68faac43a9e3f23c
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-08-12 10:07:11 +0530

    Time: 3 ms (97.52%), Space: 9 MB (5.13%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int count1Bit(uint32_t n) {
@@ -10,6 +11,33 @@ public:
     }
     
     vector<int> countBits(int n) {
+        vector<int> ans;
+        for(int i=0;i<=n;i++){
+            int currCount = count1Bit(i);
+            ans.push_back(currCount);
+        }
+        return ans;
+    }
+};
+*/
+
+// USING DP TO STORE COUNT BITS OF NUMBERS
+class Solution {
+public:
+    int dp[100000];
+    
+    int count1Bit(int n) {
+        if(n==0){
+            return 0;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n] = (n&1) + count1Bit(n>>1);
+    }
+    
+    vector<int> countBits(int n) {
+        memset(dp,-1,sizeof(dp));
         vector<int> ans;
         for(int i=0;i<=n;i++){
             int currCount = count1Bit(i);
