2768357dc0eb019f29e0998b5b478496b20b83e8
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-08-12 09:54:38 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (21.04%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int count1Bit(uint32_t n) {
+        int count = 0;
+        while(n>0){
+            count += n&1;
+            n = n>>1;
+        }
+        return count;
+    }
+    
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for(int i=0;i<=n;i++){
+            int currCount = count1Bit(i);
+            ans.push_back(currCount);
+        }
+        return ans;
+    }
+};
