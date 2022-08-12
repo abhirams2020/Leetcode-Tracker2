8874aee61d2981c0b88fd1f19ebe4eb4750d66ef
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-08-12 10:59:10 +0530

    Time: 39 ms (29.06%), Space: 17.9 MB (93.43%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,3 +1,19 @@
+/*
+// sum from 0 to n is n*(n+1)/2. subtract sum of nums from this.
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum = 0;
+        for(auto i:nums){
+            sum += i;
+        }
+        int n=nums.size();
+        return (n*(n+1)/2) - sum;
+    }
+};
+*/
+
+// BY XOR ON RANGE [0,N] AND ELEMENTS IN LIST
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
