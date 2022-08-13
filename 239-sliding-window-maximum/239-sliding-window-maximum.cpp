ad7ef7a9b4f7d02b224309231b111ac8ba37332e
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-08-13 10:12:16 +0530

    Time: 454 ms (59.38%), Space: 134.8 MB (42.01%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -6,17 +6,18 @@ public:
         int l = 0;
         for(int r=0;r<nums.size();r++){
             // keep the deque monotonic. ie for adding element at end, all elements lesser than it removed
-            while(!dq.empty() && nums[r]>nums[dq.back()]){
+            while(!dq.empty() && nums[r]>nums[dq[dq.size()-1]]){
                 dq.pop_back();
             }
             dq.push_back(r);
             
             if(r-l+1 == k){
                 // remove invalid index from dq by removing front elements less than l
-                while(!dq.empty() && dq.front() < l){
+                // after removing invalid, dq[0] will give max element in range l to r
+                while(!dq.empty() && dq[0] < l){
                     dq.pop_front();
                 }
-                maxArr.push_back(nums[dq.front()]);
+                maxArr.push_back(nums[dq[0]]);
                 l++;
             }
         }
