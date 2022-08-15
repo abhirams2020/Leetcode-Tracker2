83b0756f2aa4cbc25a65f8a099ee062fb130fb66
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-08-15 13:14:24 +0530

    Time: 16 ms (59.84%), Space: 10.8 MB (39.74%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.cpp
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
+        // if numrows == 1, no need of zigzag traversal. so just the given string
         if(numRows == 1){
             return s;
         }
+        // we create a array of strings where every row is a string
+        // till end of row, row direction is down. when end of row is reached, change row direction to up.
+        // no need of column direction since it is always in same direction. so only row index is enough.
+        vector<string> strArr(numRows);
         
-        vector<vector<char>> matrix(numRows, vector<char> (s.length(), '#')); // fill with # to show empty spaces
+        int currR = 0;
         
-        vector<vector<int>> directions = {{1,0}, {-1,1}}; // down direction and northeast diagonal direction
-        
-        int currR = 0, currC = 0;
-        
-        int dir = 0;
+        int dir = 1;
         
         int i = 0;
         
         while(i<s.length()){
-            matrix[currR][currC] = s[i];
+            strArr[currR].push_back(s[i]);
             if(currR == numRows-1){
-                dir = 1;
+                dir = -1;
             }
             else if(currR == 0){
-                dir = 0;
+                dir = 1;
             }
-            currR = currR + directions[dir][0];
-            currC = currC + directions[dir][1];
+            currR = currR + dir;
             i++;
         }
         
         string ans;
         
-        // read the matrix row by row and add non empty (not #) elements to ans
-        for(auto &v:matrix){
-            for(auto &i:v){
-                if(i!='#'){
-                    ans.push_back(i);
-                }
-            }
+        // read all rows of strArr and add the strings to ans
+        for(auto &str:strArr){
+            ans += str;
         }
         
         return ans;
