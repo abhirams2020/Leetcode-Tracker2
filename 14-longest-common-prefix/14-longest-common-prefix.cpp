c9adeea802f2c6fb474028c3c6287fb7554d4143
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-10-03 19:09:45 +0530

    Time: 15 ms (13.36%), Space: 9.2 MB (54.17%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -4,17 +4,17 @@ public:
         string ans;
         bool isMatch=true;
         for(int i=0;i<strs[0].length();i++){
-            char curr = strs[0][i]; // take every character from first word and compare 
+            char currChar = strs[0][i]; // take every character from first word and compare 
             // compare current character with i'th index for words 1 to n-1
             for(int j=1;j<strs.size();j++){
                 // if word[j] does not have i'th index or i'th index character different, ismatch = false
-                if(i==strs[j].length() || strs[j][i]!=curr){
+                if(i==strs[j].length() || strs[j][i]!=currChar){
                     isMatch = false;
                 }
             }
             // if ismatch is false, break from the loop
             if(isMatch==true){
-                ans.push_back(curr);
+                ans.push_back(currChar);
             }
             else {
                 break;
