9b5c973366501248c5b5b9dc472d3cb33f133d26
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2023-12-05 17:15:49 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -1,70 +1,41 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution {
   public:
-    vector<bool> visited;
-    
-    bool dfs(int curr, vector<int> adj[], int parent){
-        visited[curr] = true;
-        for(auto child:adj[curr]){
-            // if we havent visited a child yet, do dfs on that child and check cycle
-            if(visited[child]==false) {
-                if(dfs(child,adj,curr))
-                    return true;
+    vector<int> visited;
+    // Function to detect cycle in an undirected graph.
+    bool dfs(int curr, int prev, vector<int> adj[]){
+        visited[curr] = 1;
+        for(auto &nbr:adj[curr]){
+            if(nbr==prev){
+                continue;
             }
-            // if visited the child, check if parent of curr. if its not parent, cycle exist
-            else if(child!=parent) {
+            if(visited[nbr]==0 && dfs(nbr,curr,adj)==true){
+                return true;
+            }
+            if(visited[nbr]==1){
                 return true;
             }
         }
+        visited[curr] = 2; // curr visited and no cycle found in dfs
         return false;
     }
     
-    bool bfs(int node, vector<int> adj[]){
-        visited[node] = true;
-        // queue to store a node and its parent node
-        queue<pair<int,int>> q;
-        q.push({node,-1});
-        
-        while(!q.empty()){
-            int curr = q.front().first;
-            int parent = q.front().second;
-            q.pop();
-            for(auto child:adj[curr]){
-                // if we havent visited a child, do bfs on that child and mark visited
-                if(visited[child]==false){
-                    visited[child] = true;
-                    q.push({child,curr});
-                }
-                // if visited the child, check if parent of curr. if its not parent, cycle exist
-                else if(child!=parent){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    // Function to detect cycle in an undirected graph.
     bool isCycle(int V, vector<int> adj[]) {
-        visited.resize(V,false);
+        visited.resize(V,0);
         for(int i=0;i<V;i++){
-            if(visited[i]==false){
-                // if(dfs(i,adj,-1)){
-                //     return true;
-                // }
-                if(bfs(i,adj)){
-                    return true;
-                }
+            if(visited[i]==0 && dfs(i,-1,adj)==true){
+                return true;
             }
         }
         return false;
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 int main() {
     int tc;
     cin >> tc;
@@ -86,4 +57,5 @@ int main() {
             cout << "0\n";
     }
     return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
